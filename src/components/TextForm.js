25d747581fc001d60c322c37d571e0e8ab7b36c8
ParasPanta <Paras.pa1239@gmail.com>
2023-01-28 17:56:02 +0545

    added handle On change

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import React from "react";
 
 function TextForm() {
+  const [text, setText] = useState("");
+
+    const handleOnChange = (e) => {
+        setText(e.target.value);
+  };
+
   return (
     <>
       <div className="mb-3">
@@ -8,6 +15,8 @@ function TextForm() {
           className="form-control"
           placeholder="Enter Text Here"
           id="myBox"
+          value={text}
+          onChange={handleOnChange}
           rows="8"
         ></textarea>
       </div>
